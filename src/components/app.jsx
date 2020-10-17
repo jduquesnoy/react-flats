dafc6a16b485bb6b7fe741c27a010ba571daf1c6
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-10-17 14:36:53 +0200

    adding app.jsx

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = ({ name }) => {
+  return (
+    <div>
+      Hello, Wuuuuuuuuuuuuuurrrrrrrrlllllllllddddddd
+      {name}
+    </div>
+  );
+};
+
+
+export default App;
